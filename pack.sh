ad98d320145ea27bec27530b6d0613efe4c33c45
Jose Ojeda <joche.ojeda@bitframeworks.com>
2022-07-13 13:37:32 +0200

    updated pack.sh

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm ./nuget/Xaf.Blazor.1.0.1.nupkg
 dotnet pack template.csproj --output ./nuget
 dotnet new --uninstall Xaf.Blazor
-dotnet new -i ./nuget/Xaf.Blazor.1.0.1.nupkg
+#dotnet new -i ./nuget/Xaf.Blazor.1.0.1.nupkg
